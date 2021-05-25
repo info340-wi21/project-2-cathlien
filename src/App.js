24244aecf84d78116a473285ee0df5ab8f3daac7
cathlien <cathlien@uw.edu>
2021-05-25 14:08:03 +0800

    move images to public folder and fixed so the sample card renders

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
-export default function App() {
+export default function App(props) {
   return <div>
     <header><TopHeader /></header>
     <PurpleBlock />
     <MainNav />
     <Filter />
-    <majorCard majorCard={sampleMajorData} />
+    <MajorCard majorCard={sampleMajorData} />
     </div>;
 }
 
 export function TopHeader() {
-  return <div className="top">
+  return <div className="top" >
     <h1>
       ExploreUW
     </h1>
@@ -48,25 +48,25 @@ export function Filter() {
 
 // Sample dataset format for majorCard
 
-const sampleMajorData = {majorName: "Aquatic and Fishery Sciences", minor: true, degreeType: "bs", majorStatus: "open", imgURL: "../img/test", alt: "fish in the ocean"}
+const sampleMajorData = {majorName: "Aquatic and Fishery Sciences", minor: true, degreeType: "bs", majorStatus: "open", imgURL: "img/fish.jpeg", alt: "fish in the ocean"}
 
 
 
 // creates card for majors, need to add ability to take data from json for content and add client side routing to the links
 // Prop name is expected to be majorCard, and represent a single major object
-export function majorCard(props) {
-    let majorCard = props.majorCard
+export function MajorCard(props) {
+    let majorCard = props.majorCard;
     
     // Classes for major card, used for sorting
-    let cardClasses = "card text-center m-4" + majorCard.degreeType + majorCard.majorStatus
-    if (majorCard.minor == true) {
-      let cardClasses = cardClasses + "minor"
+    let cardClasses = "card text-center m-4" + majorCard.degreeType + majorCard.majorStatus;
+    if (majorCard.minor === true) {
+      cardClasses = cardClasses + "minor";
     }
 
     // Adding formatting for the type of degree it is and if there is a minor
     let degreeInfo = majorCard.degreeType.substring(0) + "." + majorCard.degreeType.substring(1) + "."
-    if (majorCard.minor == true) {
-      degreeInfo = degreeInfo + ", " + "Minor"
+    if (majorCard.minor === true) {
+      degreeInfo = degreeInfo + ", " + "Minor";
     }
 
     return (
