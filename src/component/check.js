b102b500ae6549351e4d2e978bdfed91ed178649
cathlien <cathlien@uw.edu>
2021-06-10 02:05:04 +0800

    fixed check list so when it switches pages the state is remembered

--- src/component/check.js
+++ src/component/check.js
@@ -1,30 +1,20 @@
 import { useState } from "react";
 
-export function Check(props) {
-    const thisCheck = props.progress;
-    
-    const [taskState, setState] = useState(thisCheck.complete);
-    
-    let changeFont = '';
-    if(taskState) {
-        changeFont = 'bg-success';
-    } else {
-        changeFont = 'bg-danger';
-    }
-
-    const handleClick = (event) => {
-        setState(!taskState);
-    }
-    return (
-        <li className={changeFont} onClick={handleClick}>
-            {thisCheck.checkText}
-        </li>
-    );
-}
-
 export function ProgressList(props) {
+    const [taskState, setState] = useState(props.progresses);
+    let handleClick = (text) => {
+        let copy = taskState.map((task) => {
+          if (task.checkText === text) {
+            task.complete = !task.complete;
+          }  return task;
+        });
+        setState(copy);
+      };
+
+
+
     let checks = props.progresses.map((eachCheck) => {
-        let thisCheck = <Check key={eachCheck.id} progress={eachCheck} />;
+        let thisCheck = <Check key={eachCheck.id} progress={eachCheck} clickCallback={handleClick}/>;
         return thisCheck;
     })
     return (
@@ -36,3 +26,24 @@ export function ProgressList(props) {
         </main>
     );
 }
+
+
+
+export function Check(props) {
+    const thisCheck = props.progress;
+    
+    
+    let changeFont = '';
+    if(thisCheck.complete) {
+        changeFont = 'bg-success';
+    } else {
+        changeFont = 'bg-danger';
+    }
+
+    return (
+        <li className={changeFont} onClick={()=> {props.clickCallback(thisCheck.checkText)}}>
+            {thisCheck.checkText}
+        </li>
+    );
+}
+
