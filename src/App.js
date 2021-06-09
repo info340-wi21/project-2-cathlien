0bd8ec3be0cfdd3b3df50c45b9dddfda6664575a
cathlien <cathlien@uw.edu>
2021-06-10 04:46:42 +0800

    trying to fix rerendering

--- src/App.js
+++ src/App.js
@@ -58,6 +58,7 @@ export default function App(props) {
         }
         unfavorite.splice(listIndex, 1);   
         setUnfavorite(unfavorite);
+        console.log("button clicked");
       };
 
   
--- src/component/cards.js
+++ src/component/cards.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { Redirect } from 'react-router-dom';
 import favorites from "./favorites";
+import useForceUpdate from 'use-force-update';
+
 
 // MajorCard returns a MajorCard component that is made up of several divs holding an image, a header, and information about the major.
 export function MajorCard(props) {
@@ -114,12 +116,88 @@ export function FavoriteCard(props) {
   return(favoriteCard);
 }
 
+
+
 export function FavoriteList(props) {
+  const [unfavorite, setUnfavorite] = useState(favorites);
+  const handleUnfavorite = (name) => {
+      let listIndex = 0;
+      for (let i = 0; i < unfavorite.length; i++) {
+        if (unfavorite[i] === name) {
+          listIndex = i;
+        }
+      }
+      unfavorite.splice(listIndex, 1);   
+      console.log(unfavorite);
+      setUnfavorite(unfavorite);
+      console.log(unfavorite);
+      console.log("button clicked");
+
+    };
+
+    console.log("favorite list running again");
+
+  const [redirectTo, setRedirectTo] = useState(undefined);
+  const handleClick = () => {
+    console.log("You clicked on", props.majorCard.majorName);
+    setRedirectTo(true);
+  }
+  
+  let favoriteCard = (card) => {
+    console.log(card);
+
+  
+    // Classes for major card, used for sorting
+    let cardClasses = "card text-center m-4" + card.degreeType + card.majorStatus;
+    if (card.minor === true) {
+      cardClasses = cardClasses + "minor";
+    }
+
+    // Adding formatting for the type of degree it is and if there is a minor
+    let degreeInfo = card.degreeType;
+    if (card.minor === true) {
+      degreeInfo = degreeInfo + ", Minor";
+    }
+
+    if (redirectTo) {
+      let link = "/major/" + card.majorName;
+      return <Redirect push to={link}/>
+    }
+
+    return (<div className={cardClasses}>
+      <img
+       className="card-img-top" src={card.imgURL} alt={card.alt}> 
+      </img>
+      <div className="card-body">
+        <h4 className="card-title text-center">{card.majorName}</h4>
+        <p className="card-text text-center"> {degreeInfo} </p>
+        <div class="btn btn-outline-success" role="button" onClick={handleClick}>Learn more</div>
+        <div class="btn btn-outline-success" role="button" onClick={() => {handleUnfavorite(card.majorName)}}>Remove from favorites</div>
+      </div>
+      <div className="card-footer text-center text-muted">Image from <a
+            href="https://unsplash.com/photos/TiTblwCHZFY"><cite>Unsplash</cite></a>
+      </div>
+    </div>)
+  }
+
+
   let cards = props.cards;
   let element = cards.map((card) => {
-    if (favorites.includes(card.majorName)) {
-      return <FavoriteCard key={card.majorName} majorCard={card} removeCallback={props.removeCallback} />;
+    if (unfavorite.includes(card.majorName)) {
+      return (favoriteCard(card))
+      
+     
     }
   })
-  return <div className="container"><div className="card-columns">{element}</div></div>
+  return <div className="container"><div className="card-columns">{
+    
+    cards.map((card) => {
+      console.log(unfavorite)
+      if (unfavorite.includes(card.majorName)) {
+        console.log(card.majorName);
+        return (favoriteCard(card))
+      }
+    })} 
+  </div>
+  </div>
 }
